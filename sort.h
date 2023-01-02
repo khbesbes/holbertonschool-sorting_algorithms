9fa9376809fd3799647bc1252654925083c91396
khbesbes <5786@holbertonstudents.com>
2023-01-02 16:13:11 +0100

    header file

---
+++ sort.h
@@ -0,0 +1,14 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+#include "sort.h"
+#include <sort.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
+
