33b4858fd2cc574a09758c1b4e7a084a0492d96c
khbesbes <5786@holbertonstudents.com>
2023-01-05 18:49:50 +0100

    hello

--- sort.h
+++ sort.h
@@ -23,7 +23,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *a, int *b);
+void swap(int *a, int *b); 
+size_t partition(int array[], size_t size);
+void print_sort(int array[], size_t size, int init);
 
 #endif
 
